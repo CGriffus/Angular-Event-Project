824239254d2c4177f048e7809088d3f70acae7b4
Caitlin <caitlin@griffus.com>
2019-08-19 14:18:18 -0400

    added components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { BucketlistPageComponent } from './bucketlist-page/bucketlist-page.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCriteriaComponent,
+    EventListComponent,
+    BucketlistPageComponent,
+    EventDetailComponent
   ],
   imports: [
     BrowserModule
