301f1e5f8a9fd7655a4363619ba03b3a809f6c79
Caitlin <caitlin@griffus.com>
2019-08-19 14:46:26 -0400

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { SearchCriteriaComponent } from "./search-criteria/search-criteria.component";
+import { EventListComponent } from "./event-list/event-list.component";
+import { BucketlistPageComponent } from "./bucketlist-page/bucketlist-page.component";
+import { EventDetailComponent } from "./event-detail/event-detail.component";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { BucketlistPageComponent } from './bucketlist-page/bucketlist-page.component';
-import { EventDetailComponent } from './event-detail/event-detail.component';
+const appRoutes: Routes = [
+  { path: "eventDetail", component: EventDetailComponent },
+  { path: "eventList", component: EventListComponent },
+  { path: "bucketList", component: BucketlistPageComponent },
+  { path: "search", component: SearchCriteriaComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,9 +25,12 @@ import { EventDetailComponent } from './event-detail/event-detail.component';
     EventDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
