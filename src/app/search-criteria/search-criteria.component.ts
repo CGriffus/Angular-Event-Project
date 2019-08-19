b5bc8d3141d51cafae7cee8bb33730ec9b8ba90e
Caitlin <caitlin@griffus.com>
2019-08-19 16:50:02 -0400

    form is submitting

--- src/app/search-criteria/search-criteria.component.ts
+++ src/app/search-criteria/search-criteria.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { EventsService } from "../services/events.service";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-search-criteria",
@@ -7,7 +8,24 @@ import { EventsService } from "../services/events.service";
   styleUrls: ["./search-criteria.component.css"]
 })
 export class SearchCriteriaComponent implements OnInit {
+  eventData: any;
+
   constructor(private eventsService: EventsService) {}
 
   ngOnInit() {}
+
+  getEvents(form: NgForm): void {
+    this.eventsService
+      .getEventData(
+        form.value.keyword
+        // form.value.location,
+        // form.value.startDate,
+        // form.value.endDate
+      )
+      .subscribe(response => {
+        this.eventData = response.data;
+        console.log(this.eventData);
+        console.log(form);
+      });
+  }
 }
