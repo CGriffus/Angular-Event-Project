d182453f4b2ad3d0440f316330935f2c0f9b2b41
Caitlin <caitlin@griffus.com>
2019-08-21 08:47:39 -0400

    nested components in home

--- src/app/home/sidebar/search-criteria/search-criteria.component.ts
+++ src/app/home/sidebar/search-criteria/search-criteria.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { EventsService } from "../services/events.service";
+import { EventsService } from "../../../services/events.service";
 import { NgForm } from "@angular/forms";
 
 @Component({
